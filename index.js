6861c0f3662a1303ad3ad8c0f5538fd5398da327
malavikavenugopal <malavikavenu914@gmail.com>
2024-01-22 11:37:43 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//import json-server
+
+const jsonServer = require('json-server')
+
+//create json-server
+
+const employeeServer = jsonServer.create()
+
+
+//set path for json file
+const router = jsonServer.router('db.json')
+
+
+//middleware
+const middleware = jsonServer.defaults()
+
+
+//server use middleware and router
+employeeServer.use(middleware)
+employeeServer.use(router)
+
+//setup the port
+
+const port = 3000 || process.env.PORT
+
+
+employeeServer.listen(port,()=>{
+
+    console.log(`EmployeeServer is running successfully at port number ${port}`);
+})
